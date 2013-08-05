27cae071277f3c883f48effe2b58f105776ed694
Alex Chuchunkov <scarywound@gmail.com>
2013-08-05 21:20:34 +0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-source /home/alex/.rvm/environments/ruby-1.9.3-p448
+# If you use RVM instead of system-wide Ruby (like I do), this line is for you:
+# source /home/your-username/.rvm/environments/ruby-1.9.3-p448
 
 /home/alex/projects/github-explore-mailing/job.rb
